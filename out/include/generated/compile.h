f6b7d1715f49e19683affe0b2a8ca07a4cbc60e1
maosith <mns.ao@proton.me>
2023-10-21 13:50:37 -0300

    fix: bring back staging missing files from my source.

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Apr 27 23:37:29 -03 2024"
+#define LINUX_COMPILE_BY "msouto"
+#define LINUX_COMPILE_HOST "debianmao"
+#define LINUX_COMPILER "Neutron clang version 17.0.0 (https://github.com/llvm/llvm-project.git 9aae408d551083bbbac96ecc512d45c30358e4b9), Neutron LLD 17.0.0"
